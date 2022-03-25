eb00f74fe5a4ecf0c70e22497ea445644bd5f95e
Robinsoncifuentes <robinsonmunoz4@gmail.com>
2022-03-25 21:52:12 +0000

    en dev

--- src/main.py
+++ src/main.py
@@ -8,38 +8,327 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, Favoritcharacter, Favoritplanet, Favoritspecie, Favoritstarship, Character, Starship, Planet, Specie
+
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['ENV'] = 'development'
 app.url_map.strict_slashes = False
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///dev_4geeks.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
 
-# Handle/serialize errors like a JSON object
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
-@app.route('/')
-def sitemap():
-    return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+db.init_app(app)
+Migrate(app, db)
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/users', methods=['GET'])
+def all_user():
+    users = User.query.all()
+    users = list(map(lambda user: user.serialize(), users))
+    return jsonify(users), 200
+    
 
-    return jsonify(response_body), 200
+@app.route('/users/<int:id>', methods=['GET'])
+def get_user(id):
+    users = User.query.get(id)
+    return jsonify(users.serialize()), 200
 
-# this only runs if `$ python src/main.py` is executed
+@app.route('/users', methods=['POST']) 
+def create_user():
+    name = request.json.get('name')
+    email = request.json.get('email')
+
+    user = User()
+    user.name = name
+    user.email = email
+
+    db.session.add(user)
+    db.session.commit()
+    
+    return jsonify(user.serialize()), 201
+
+@app.route('/users/<int:id>', methods=['PUT']) 
+def modificar_user(id):
+    name = request.json.get('name')
+    email = request.json.get('email')
+
+    user = User.query.get(id)
+    user.name = name
+    user.email = email
+
+    
+    db.session.commit()
+    
+    return jsonify(user.serialize()), 201
+
+@app.route('/users/<int:id>', methods=['DELETE'])
+def delete_users(id):
+    users = User.query.get(id)
+    db.session.delete(users)
+    db.session.commit()
+
+    return jsonify({}), 200   
+
+@app.route('/character', methods=['GET'])
+def get_character():
+    characters = Character.query.all()
+    characters = list(map(lambda character: character.serialize(), characters))
+    return jsonify(characters), 200
+
+@app.route('/character/<int:id>', methods=['GET'])
+def get_character_by_id(id):
+    character = Character.query.get(id)
+    return jsonify(character.serialize()), 200
+
+@app.route('/character', methods=['POST'])
+def create_character():
+    name = request.json.get('name')
+    lastname = request.json.get('lastname')
+    homeworld = request.json.get('homeworld')
+    gender = request.json.get('gender')
+    specie = request.json.get('specie')  
+
+    character = Character()
+    character.name = name
+    character.lastname = lastname
+    character.homeworld = homeworld
+    character.gender = gender
+    character.specie = specie
+
+    db.session.add(character)
+    db.session.commit()
+
+    return jsonify(character.serialize()), 201
+
+@app.route('/character/<int:id>', methods=['PUT'])
+def update_character(id):
+     name = request.json.get('name')
+     lastname = request.json.get('lastname')
+     homeworld = request.json.get('homeworld')
+     gender = request.json.get('gender')
+     specie = request.json.get('specie')
+
+     character = Character.query.get(id)
+     character.name = name
+     character.lastname = lastname
+     character.homeworld = homeworld
+     character.gender = gender
+     character.specie = specie
+
+     db.session.commit()
+
+     return jsonify(character.serialize()), 200
+
+@app.route('/character/<int:id>', methods=['DELETE'])
+def delete_character(id):
+    character = Character.query.get(id)
+    db.session.delete(character)
+    db.session.commit()
+
+    return jsonify({}), 200
+                  
+
+
+@app.route('/planet', methods=['GET'])
+def get_planet():
+    planets = Planet.query.all()
+    planets = list(map(lambda planet: planet.serialize(), planets))
+    return jsonify(planets), 200
+
+@app.route('/planet/<int:id>', methods=['GET'])
+def get_planet_by_id(id):
+    planet = Planet.query.get(id)
+    return jsonify(planet.serialize()), 200
+
+@app.route('/planet', methods=['POST'])
+def create_planet():
+    name = request.json.get('name')
+    climate = request.json.get('climate')
+    population = request.json.get('population')
+   
+
+    planet = Planet()
+    planet.name = name
+    planet.climate = climate
+    planet.population = population
+    
+
+    db.session.add(planet)
+    db.session.commit()
+
+    return jsonify(planet.serialize()), 201
+
+@app.route('/planet/<int:id>', methods=['PUT'])
+def update_planet(id):
+     name = request.json.get('name')
+     climate = request.json.get('climate')
+     population = request.json.get('population')
+     
+
+     planet = Planet.query.get(id)
+     planet.name = name
+     planet.climate = climate
+     planet.population = population
+     
+
+     db.session.commit()
+
+     return jsonify(planet.serialize()), 200
+
+@app.route('/planet/<int:id>', methods=['DELETE'])
+def delete_planet(id):
+    planet = Planet.query.get(id)
+    db.session.delete(planet)
+    db.session.commit()
+
+    return jsonify({}), 200
+
+
+@app.route('/specie', methods=['GET'])
+def get_specie():
+    species = Specie.query.all()
+    species = list(map(lambda specie: specie.serialize(), species))
+    return jsonify(species), 200
+
+@app.route('/specie/<int:id>', methods=['GET'])
+def get_specie_by_id(id):
+    specie = Specie.query.get(id)
+    return jsonify(specie.serialize()), 200
+
+@app.route('/specie', methods=['POST'])
+def create_specie():
+    name = request.json.get('name')
+    homeworld = request.json.get('homeworld')
+    lenguage = request.json.get('lenguage')
+     
+
+    specie = Specie()
+    specie.name = name
+    specie.homeworld = homeworld
+    specie.lenguage = lenguage
+   
+
+    db.session.add(specie)
+    db.session.commit()
+
+    return jsonify(specie.serialize()), 201
+
+@app.route('/specie/<int:id>', methods=['PUT'])
+def update_specie(id):
+     name = request.json.get('name')
+     homeworld = request.json.get('homeworld')
+     lenguage = request.json.get('lenguage')
+     
+
+     specie = Specie.query.get(id)
+     specie.name = name
+     specie.homeworld = homeworld
+     specie.lenguage = lenguage
+    
+
+     db.session.commit()
+
+     return jsonify(specie.serialize()), 200
+
+@app.route('/specie/<int:id>', methods=['DELETE'])
+def delete_specie(id):
+    specie = Specie.query.get(id)
+    db.session.delete(specie)
+    db.session.commit()
+
+    return jsonify({}), 200
+
+
+@app.route('/starship', methods=['GET'])
+def get_starship():
+    starships = Starship.query.all()
+    starships = list(map(lambda starship: starship.serialize(), starships))
+    return jsonify(starships), 200
+
+@app.route('/starship/<int:id>', methods=['GET'])
+def get_starship_by_id(id):
+    starship = Starship.query.get(id)
+    return jsonify(starship.serialize()), 200
+
+@app.route('/starship', methods=['POST'])
+def create_starship():
+    name = request.json.get('name')
+    model = request.json.get('model')
+    manufacturer = request.json.get('manufacturer')
+    
+
+    starship = Starship()
+    starship.name = name
+    starship.model = model
+    starship.manufacturer = manufacturer
+    
+    db.session.add(starship)
+    db.session.commit()
+
+    return jsonify(starship.serialize()), 201
+
+@app.route('/starship/<int:id>', methods=['PUT'])
+def update_starship(id):
+     name = request.json.get('name')
+     model = request.json.get('model')
+     manufacturer = request.json.get('manufacturer')
+     
+
+     starship = Starship.query.get(id)
+     starship.model = model
+     starship.manufacturer = manufacturer
+
+     db.session.commit()
+
+     return jsonify(starship.serialize()), 200
+
+@app.route('/starship/<int:id>', methods=['DELETE'])
+def delete_starship(id):
+    starship = Starship.query.get(id)
+    db.session.delete(starship)
+    db.session.commit()
+
+    return jsonify({}), 200
+
+
+@app.route('/favoritcharacter', methods=['GET'])
+def get_fav_character():
+    fav_characters = Favoritcharacter.query.all()
+    fav_characters = list(map(lambda fav_character: fav_character.serialize(), fav_characters))
+    return jsonify(fav_characters), 200
+
+@app.route('/favoritcharacter/<int:id>', methods=['GET'])
+def get_fav_character_by_id(id):
+    fav_character = Favoritcharacter.query.get(id)
+    return jsonify(fav_character.serialize()), 200
+
+@app.route('/favoritcharacter/character/<int:id>', methods=['POST'])
+def create_fav_character():
+    name = request.json.get('name')
+    lastname = request.json.get('lastname')
+    homeworld = request.json.get ('homeworld')
+    gender = request.json.get('gender')
+    specie = request.json.get('specie')
+
+    fav_character = Favoritcharacter()
+    fav_character.name = name
+    fav_character.lastname = lastname
+    fav_character.homeworld = homeworld
+    fav_character.gender = gender
+    fav_character.specie = specie
+
+    db.session.add(fav_character)
+    db.session.commit()
+    return jsonify(fav_character.serialize()), 201
 if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
+    app.run(host="0.0.0.0", port=3452)
--- src/models.py
+++ src/models.py
@@ -1,19 +1,235 @@
 from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy import Column, ForeignKey, Integer, String
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "email": self.email,
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    def update(self):
+        db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()        
+
+
+
+
+
+class Favoritcharacter(db.Model):
+    __tablename__ = 'favoritcharacter'
+    id = db.Column(db.Integer, primary_key=True)
+    characters_id = db.Column(db.Integer, ForeignKey('character.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    favorit_character = db.relationship('User')
+    favorit_character2 = db.relationship('Character')
+    
+    
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "user_id": self.user_id,
+            "characters_id": self.characters_id
         }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    def update(self):
+        db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()    
+
+class Favoritplanet(db.Model):
+    __tablename__ = 'favoritplanet'
+    id = db.Column(db.Integer, primary_key=True)
+    planet_id = db.Column(db.Integer, ForeignKey('planet.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    pl = db.relationship('User')
+    pl1 = db.relationship('Planet')
+    
+    
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "planet_id": self.planet_id
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    def update(self):
+        db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()    
+
+class Favoritspecie(db.Model):
+    __tablename__ = 'favoritspecie'
+    id = db.Column(db.Integer, primary_key=True)
+    specie_id = db.Column(db.Integer, ForeignKey('specie.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    sp = db.relationship('User')
+    sp1 = db.relationship('Specie')
+    
+
+    
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "specie_id": self.specie_id
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    def update(self):
+        db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()    
+
+class Favoritstarship(db.Model):
+    __tablename__ = 'favoritstarship'
+    id = db.Column(db.Integer, primary_key=True)
+    starship_id = db.Column(db.Integer, ForeignKey('starship.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    st = db.relationship('User')
+    st = db.relationship('Starship')
+    
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "starship_id": self.starship_id
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    def update(self):
+        db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()    
+
+class Character(db.Model):
+    __tablename__ = 'character'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    lastname = db.Column(db.String(250), nullable=False)
+    homeworld = db.Column(db.String(250), nullable=False)
+    gender = db.Column(db.String(250), nullable=False)
+    specie = db.Column(db.String(250), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "lastname": self.lastname,
+            "homeworld": self.homeworld,
+            "gender": self.gender,
+            "specie": self.specie
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    def update(self):
+        db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()    
+
+class Planet(db.Model):
+    __tablename__ = 'planet'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    climate = db.Column(db.String(250), nullable=False)
+    population = db.Column(db.String(250), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            "population": self.population
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    def update(self):
+        db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()    
+
+class Specie(db.Model):
+    __tablename__ = 'specie'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    homeworld = db.Column(db.String(250), nullable=False)
+    lenguage = db.Column(db.String(250), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "homeworld": self.homeworld,
+            "lenguage": self.lenguage
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    def update(self):
+        db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()    
+
+class Starship(db.Model):
+    __tablename__ = 'starship'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    model = db.Column(db.String(250), nullable=False)
+    manufacturer = db.Column(db.String(250), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "model": self.model,
+            "manufacturer": self.manufacturer
+        } 
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    def update(self):
+        db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()             
